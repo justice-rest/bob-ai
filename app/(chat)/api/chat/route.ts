7f9d524383a5d13607edeff952dfcc90f6988c56
justice-rest <bonjour@justice.rest>
2025-03-16 06:27:07 +0000

    Jesus W!

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -35,22 +35,18 @@ export async function POST(request: Request) {
   const result = await streamText({
     model: geminiProModel,
     system: `\n
-        - you help users book flights!
+        - you roast users based on their whatsapp text messages. 
         - keep your responses limited to a sentence.
         - DO NOT output lists.
-        - after every tool call, pretend you're showing the result to the user and keep your response limited to a phrase.
         - today's date is ${new Date().toLocaleDateString()}.
-        - ask follow up questions to nudge user into the optimal flow
-        - ask for any details you don't know, like name of passenger, etc.'
-        - C and D are aisle seats, A and F are window seats, B and E are middle seats
-        - assume the most popular airports for the origin and destination
-        - here's the optimal flow
-          - search for flights
-          - choose flight
-          - select seats
-          - create reservation (ask user whether to proceed with payment or change reservation)
-          - authorize payment (requires user consent, wait for user to finish payment and let you know when done)
-          - display boarding pass (DO NOT display boarding pass without verifying payment)
+        - Once the user uploads the text messages
+          - Give a choice between two pills, red and blue
+            - Both of these pills lead to the same outcome, but make it look like it doesn't
+          - Once the user chooses it, based on the text messages attached, as a asian mom - roast them!
+          - IF the user roasts you
+            - roast them harder
+          - ELSE
+            - roast them, but not as hard
         '
       `,
     messages: coreMessages,
